a939b428fa85eee9d9fa167c3286af97358e14cd
rayolive <120057654+rayolive@users.noreply.github.com>
2023-03-25 12:00:16 +0530

    Update socket_1.py

--- socket_1.py
+++ socket_1.py
@@ -4,6 +4,6 @@ serverPORT = 6000
 serverAddress = (serverIP,serverPORT)
 bufferSize = 1024
 UDPClientSocket = socket.socket(family=socket.AF_INET,type=socket.SOCK_DGRAM)
-message = "Hi, My name is Debrishi 2022A4PS0902P"
+message = "Hi, My name is Olive 2022B4PS0681P"
 bytetosend =  str.encode(message)
 UDPClientSocket.sendto(bytetosend,serverAddress)
